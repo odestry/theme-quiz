9f762e36e61ac71bbeb7ece4d19c973acdd34dfe
Mooh07 <nvm2K213@gmail.com>
2024-01-02 16:19:11 +0100

    SSR quiz

--- assets/global.js
+++ assets/global.js
@@ -1,139 +1,113 @@
-const quizManager = {
-  data: {},
-  setData: (key, value) => {
-    quizManager.data[key] = value
-  },
-  getData: () => {
-    return [...new Set(Object.values(quizManager.data))]
-  },
-  bindDataToForm: () => {
-    const input = document.querySelector('[name="contact[tags]"]')
-    if (!input) return
-
-    input.value = quizManager.getData().join(',')
-  },
-  getRecommendedProduct: async () => {
-    // TODO: Get the recommended product based on diffrent approches
-    // You can use an API like Open AI or a JSON file (Metobjects)
-    // to return a product ID based on the quiz data.
-    const searchTerm = quizManager.getData().join(' ')
-
-    const response = await fetch(
-      `${routes.predictive_search_url}.json?q=${encodeURIComponent(
-        searchTerm
-      )}&resources[type]=product`
-    )
-    const suggestions = await response.json()
-
-    if (suggestions.resources.results.products.length) {
-      return suggestions.resources.results.products[0].id
-    }
-  }
-}
-
 customElements.define(
-  'quiz-router',
+  'quiz-step',
   class extends HTMLElement {
     constructor() {
-      super()
-      this.state = {
-        currentStep: parseInt(this.dataset.currentStep),
-        nextStep: parseInt(this.dataset.nextStep)
-      }
-      if (!this.state.nextStep) {
-        this.renderProductRecommendations()
-        quizManager.bindDataToForm()
-        return
-      }
+      super();
+      this.quizManager = localStorage.getItem('quiz_manager');
+      if (!this.quizManager) this.quizManager = {};
+      else this.quizManager = JSON.parse(this.quizManager);
 
-      this.buttons = this.querySelectorAll('[type="button"]')
-      if (this.buttons.length < 1) return
-      this.buttons.forEach(button =>
-        button.addEventListener('click', this.switch.bind(this))
-      )
+      this.setQuizzData();
+      this.querySelectorAll('.next-step-button').forEach((item) => {
+        if (this.isFinalStep) item.addEventListener('click', this.handleLastStep.bind(this));
+        else item.addEventListener('click', this.handleStepChange.bind(this));
+      });
+      if (this.isFinalStep && document.getElementById('quiz-contact')) {
+        this.quizContactContainer = document.getElementById('quiz-contact');
+        this.ajaxifyAndDisplayForm();
+      }
     }
+    ajaxifyAndDisplayForm() {
+      const form = this.quizContactContainer.querySelector('form');
+      form.addEventListener('submit', (event) => {
+        event.stopPropagation();
+        event.preventDefault();
 
-    async switch(event) {
-      const { currentStep, nextStep } = this.state
-      const button = event.target
-      button.disabled = true
+        const form = event.target;
+        const formData = new FormData(form);
+        const action = form.getAttribute('action');
+        fetch(action, {
+          method: 'POST',
+          body: formData,
+        })
+          .then((response) => {
+            if (response.ok) {
+              this.quizContactContainer.classList.add('hidden');
+              this.hideProgressShowResults();
+              return;
+            }
+            throw new Error('Network response was not ok.');
+          })
+          .catch((error) => {
+            console.error('There was a problem with the fetch operation:', error);
+          });
+      });
+    }
+    handleLastStep(e) {
+      e.preventDefault();
+      this.setChoice(e.currentTarget);
+      document.querySelector('progress').value = 100;
+      this.querySelector('.final-step-container').classList.add('hidden');
+      this.querySelector('.results-wrapper').classList.remove('hidden');
 
-      if (button.dataset.value) {
-        quizManager.setData(`step-${currentStep}`, button.dataset.value)
+      if (this.quizContactContainer) {
+        this.quizContactContainer.classList.remove('hidden');
+        this.bindDataToForm();
+      } else {
+        this.hideProgressShowResults();
       }
+
+      this.renderResults();
+    }
+    hideProgressShowResults() {
+      document.querySelector('progress').classList.add('hidden');
+      this.querySelector('.results-container').classList.remove('hidden');
+    }
+    renderResults() {
+      this.getRecommendedProduct().then(async (productId) => {
+        const recommendedSectionFetchURL = `${window.routes.product_recommendations_url}?section_id=product-finder-recommended&product_id=${productId}&limit=6`;
+        const recommendedResponse = await fetch(recommendedSectionFetchURL);
+        const recommendedText = await recommendedResponse.text();
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(recommendedText, 'text/html');
+        const recommendedResults = doc.querySelector('.results');
+        document.querySelector('.results').innerHTML = recommendedResults.innerHTML;
+      });
+    }
+    setQuizzData() {
+      const { step, quiz, final } = this.dataset;
+      if (final) this.isFinalStep = true;
+      if (!this.quizManager[quiz]) this.quizManager[quiz] = {};
+      this.step = step;
+      this.quiz = quiz;
+    }
+    handleStepChange(e) {
+      this.setChoice(e.currentTarget);
+      localStorage.setItem('quiz_manager', JSON.stringify(this.quizManager));
+    }
+    setChoice(htmlAnchorTagClicked) {
+      const { choiceValue } = htmlAnchorTagClicked.dataset;
+      this.quizManager[this.quiz][this.step] = choiceValue;
+    }
+    getData() {
+      return [...new Set(Object.values(this.quizManager[this.quiz]))];
+    }
+    bindDataToForm() {
+      const input = this.quizContactContainer.querySelector('[name="contact[tags]"]');
+      if (!input) return;
+      input.value = this.getData().join(',');
+    }
+    async getRecommendedProduct() {
+      let searchTerm = this.getData().join(' OR ');
 
       const response = await fetch(
-        `${window.location.pathname}?sections=${
-          this.getSectionToRender(nextStep).id
-        }`,
-        this.fetchConfig()
-      )
-      const parsedState = await response.json()
-      this.renderContents(parsedState, currentStep)
-      history.pushState(
-        { currentStep, nextStep },
-        `Step ${nextStep}`,
-        `${window.location.pathname}?step=${nextStep}`
-      )
-    }
+        `${routes.predictive_search_url}.json?q=${encodeURIComponent(searchTerm)}&resources[type]=product`
+      );
+      const suggestions = await response.json();
 
-    renderContents(parsedState, step) {
-      const section = this.getSectionToRender(step),
-        newSection = this.getSectionToRender(step + 1)
-      const sectionElement = section.selector
-        ? document.querySelector(section.selector)
-        : document.getElementById(section.id)
-      sectionElement.parentElement.innerHTML = this.getSectionInnerHTML(
-        parsedState[newSection.id]
-      )
-    }
-
-    getSectionInnerHTML(html, selector = '.shopify-section') {
-      return new DOMParser()
-        .parseFromString(html, 'text/html')
-        .querySelector(selector).innerHTML
-    }
-
-    getSectionToRender(step) {
-      return {
-        id: step === 0 ? 'main-quiz' : `quiz-step-${step}`,
-        selector: step === 0 ? '.main-quiz' : `.quiz-step-${step}`
+      if (suggestions.resources.results.products.length) {
+        return suggestions.resources.results.products[0].id;
       }
-    }
-
-    fetchConfig(type = 'json') {
-      return { method: 'GET', headers: { Accept: `application/${type}` } }
-    }
-
-    renderProductRecommendations() {
-      const recommendationsElement = document.querySelector('.results')
-      const handleIntersection = (entries, observer) => {
-        if (!entries[0].isIntersecting) return
-        observer.unobserve(this)
-
-        quizManager.getRecommendedProduct().then(productId => {
-          const fetchURL = `${
-            window.routes.product_recommendations_url
-          }?section_id=${
-            this.getSectionToRender(this.state.currentStep).id
-          }&product_id=${productId}&limit=6`
-
-          fetch(fetchURL)
-            .then(response => response.text())
-            .then(text => {
-              const html = document.createElement('div')
-              html.innerHTML = text
-              const recommendations = html.querySelector('.results')
-              if (recommendations && recommendations.innerHTML.trim().length) {
-                recommendationsElement.innerHTML = recommendations.innerHTML
-              }
-            })
-        })
-      }
-
-      new IntersectionObserver(handleIntersection.bind(this), {
-        rootMargin: '0px 0px 400px 0px'
-      }).observe(this)
     }
   }
-)
+);
